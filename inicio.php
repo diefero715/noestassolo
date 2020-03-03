0901550a2ffcea69bcaf7eb428bd5cef9d006582
diefero715 <61721255+diefero715@users.noreply.github.com>
2020-03-03 10:38:13 -0500

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -62,57 +62,6 @@
         </div>
     </nav>
 
-    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Nueva Nota</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm">
-                                                            <form>
-                                    <div class="form-group">
-                                        <label for="exampleFormControlFile1">Subir imagen de la nota (test)</label>
-                                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                                    </div>
-                                </form>
-
-                            </div>
-                            <div class="col-sm">
-                                <form>
-                                    <div class="form-group">
-                                        <label for="exampleInputTitle">Titulo de la nota</label>
-                                        <input type="text" class="form-control" id="exampleInputTitle"
-                                            placeholder="Ingrese un titulo">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleFormControlTextarea1">Descripción</label>
-                                        <textarea class="form-control no-resize" id="exampleFormControlTextarea1"
-                                            rows="4" placeholder="Ingrese una descripción"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="input">Fecha</label>
-                                        <input type="date" class="form-control" placeholder="Ingrese la fecha">
-                                    </div>                                                             
-                                </form>                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary">Guardar Nota</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="container">
 
         <h4 class="font-media mt-5 mb-0">Lo más visto el último mes</h4>
